fb3e23c57f4ad9335da049e27f1c1ff7fe985889
Erick Neves <neves.erick@gmail.com>
2013-10-14 22:19:07 -0300

    updating to clj-kafka-0.0.8

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject kafka-ex01 "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-		[clj-kafka "0.0.5-0.7-SNAPSHOT"]])
+  		[com.yammer.metrics/metrics-core "2.2.0"]
+  		[com.101tec/zkclient "0.3"]
+		[clj-kafka "0.1.0-0.8-beta1"]])
--- src/kafka_ex01/core.clj
+++ src/kafka_ex01/core.clj
@@ -1,13 +1,57 @@
 (ns kafka-ex01.core
-(:use clj-kafka.producer)
-(:use clj-kafka.consumer.simple))
+  (:use [clj-kafka.core :only [with-resource]])
+  (:require [clj-kafka.producer :as p])
+  (:require [clj-kafka.zk :as zk])
+  (:require [clj-kafka.consumer.simple :as s])
+  (:require [clj-kafka.consumer.zk :as zkc]))
 
-(def p (producer {"zk.connect" "localhost:2181"}))
 
-(defn send-item [item] (send-messages p "test" (message (.getBytes item))))
+(def zk-config {"zookeeper.connect" "localhost:2181"})
+(defn brokers
+  "Get brokers from zookeeper"
+  [] (zk/brokers zk-config))
 
-(def c (consumer "localhost" 9092))
-(def f (fetch "test" 0 0 4096))
-(messages c f)
+(defn controller
+  "Get leader node"
+  [] (zk/controller zk-config))
 
-(defn get-item [] (messages c f))
+(defn topics
+  "Get topics"
+  [] (zk/topics zk-config))
+
+(def producer (p/producer {"metadata.broker.list" "localhost:9092"
+                      "serializer.class" "kafka.serializer.DefaultEncoder"
+                      "partitioner.class" "kafka.producer.DefaultPartitioner"}))
+
+(def topic "test")
+(defn send-item [item] (p/send-message producer (p/message topic (.getBytes item))))
+
+
+(def client-id "cli-id")
+(def simple-consumer (s/consumer "localhost" 9092 client-id))
+(defn get-items [] (let [part 0
+                         offset 0
+                         fetch-size 4096]
+                     (s/messages simple-consumer client-id topic part offset fetch-size)))
+
+(defn do-for-each [item] (-> item :value (String.) prn))
+
+(defn consume-item-batch-and-process [batch-size]
+  (let [consumer-config (merge zk-config
+                               {"group.id" "clj-kafka.consumer"
+                                "auto.offset.reset" "smallest"
+                                "zookeeper.session.timeout.ms" "400"
+                                "zookeeper.sync.time.ms" "200"
+                                "auto.commit.interval.ms" "1000"
+                                "auto.commit.enable" "true"})]
+    (with-resource [consumer (zkc/consumer consumer-config)]
+      zkc/shutdown
+      (doall
+       (pmap do-for-each (take batch-size (zkc/messages consumer [topic])))))))
+
+(def switch (atom true)) ; a switch to stop workers
+
+(defn run! []
+  (while @switch
+    (Thread/sleep 1000)
+    (do (consume-item-batch-and-process 2))))
