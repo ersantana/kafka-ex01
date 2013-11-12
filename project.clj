c34fa559ea42ad19fdfba3641e4b413c5a1da314
Erick Neves <neves.erick@gmail.com>
2013-11-12 15:46:37 -0200

    Revert "updating to clj-kafka-0.0.8"
    This reverts commit fb3e23c57f4ad9335da049e27f1c1ff7fe985889.

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject kafka-ex01 "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-  		[com.yammer.metrics/metrics-core "2.2.0"]
-  		[com.101tec/zkclient "0.3"]
-		[clj-kafka "0.1.0-0.8-beta1"]])
+		[clj-kafka "0.0.5-0.7-SNAPSHOT"]])
